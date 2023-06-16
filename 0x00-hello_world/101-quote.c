4cfa836a29b088f24eb33b9c647ea48632100d6b
DINOBOI7 <taiwodav18@gmail.com>
2023-06-15 20:35:00 -0700

    Hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+ * main - Entry  point 
+ * 
+ * Return : Always 0 (success)
+ */
+int main (0)
+{
+ write(2,
+ "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+ return (1)
+ }
