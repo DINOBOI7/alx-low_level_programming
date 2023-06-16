830d444c5aefad5f367a09ea4c50485eaf1ace3a
DINOBOI7 <taiwodav18@gmail.com>
2023-06-16 03:37:48 -0700

    Hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,22 @@
-#!/bin/bash
-#include<stdio.h>
-/**
- * main - Entry  point 
- * 
- * Return : Always 0 (success)
+/*
+ * File: 5-printf.c
+ * Auth: Brennan D Baraban
  */
-int main (void)
+
+
+#include <stdio.h>
+
+
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+
 	return (0);
 }
